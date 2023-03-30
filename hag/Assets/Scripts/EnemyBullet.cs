645639d03cef784d26f3dadd6212e2d15716ad53
HaSenugJin <126733434+HaSenugJin@users.noreply.github.com>
2023-03-30 21:53:26 +0900

    Bullet

--- hag/Assets/Scripts/EnemyBullet.cs
+++ hag/Assets/Scripts/EnemyBullet.cs
@@ -9,36 +9,34 @@ public class EnemyBullet : MonoBehaviour
     // ** 총알이 충돌한 횟수
     private int hp;
 
-    // ** 이펙트효과 원본
-    //public GameObject fxPrefab;
+    private GameObject Target;
 
     // ** 총알이 날아가야할 방향
     public Vector3 Direction { get; set; }
+
+    private void Awake()
+    {
+        Target = GameObject.Find("Player");
+    }
 
     private void Start()
     {
         // ** 속도 초기값
         Speed = ControllerManager.GetInstance().BulletSpeed;
 
-        // ** 충돌 횟수를 3으로 지정한다.
+        // ** 충돌 횟수를 지정한다.
         hp = 1;
     }
 
     private void Update()
     {
         // ** 방향으로 속도만큼 위치를 변경
-        transform.position += Direction * Speed * Time.deltaTime;
+        Target.transform.position -= transform.position * Speed * Time.deltaTime;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // ** 충돌횟수 차감.
         --hp;
-
-        // ** 이펙트효과 복제.
-        //GameObject Obj = Instantiate(fxPrefab);
-
-        // ** 이펙트효과의 위치를 지정
-        //Obj.transform.position = transform.position;
 
         // ** collision = 충돌한 대상. 
         if (collision.transform.tag == "Player")
--- hag/Assets/Scripts/EnemyController.cs
+++ hag/Assets/Scripts/EnemyController.cs
@@ -7,23 +7,22 @@ public class EnemyController : MonoBehaviour
     public GameObject Target;
     public float CoolDown;
     public float Speed;
-
+    private GameObject BulletPrefab;
     private int HP;
     private Animator Anim;
     private Vector3 Movement;
-    private GameObject Obj;
+
     private float Direction;
     private bool Attack;
-    private GameObject fxPrefab;
+
     private List<GameObject> Bullets = new List<GameObject>();
-    private GameObject BulletPrefab;
+
 
     private void Awake()
     {
         Target = GameObject.Find("Player");
-
         BulletPrefab = Resources.Load("Prefabs/Enemy/EnemyBullet") as GameObject;
-        fxPrefab = Resources.Load("Prefabs/FX/Hit") as GameObject;
+
         Anim = GetComponent<Animator>();
     }
 
@@ -45,14 +44,13 @@ public class EnemyController : MonoBehaviour
         {
             Attack = true;
             Anim.SetTrigger("Skill");
-            StartCoroutine(OnAttack());
         }
         else if (Attack)
         {
-           
+
             Anim.SetFloat("Speed", Movement.x);
             transform.position -= Movement * Time.deltaTime;
-            
+        
         }
     }
 
@@ -77,39 +75,24 @@ public class EnemyController : MonoBehaviour
 
     private void attack()
     {
-        //Attack = false;
+        Attack = false;
     }
 
-    IEnumerator OnAttack()
+    private void BU()
     {
-        // ** 총알원본을 본제한다.
         GameObject Obj = Instantiate(BulletPrefab);
 
         // ** 복제된 총알의 위치를 현재 플레이어의 위치로 초기화한다.
         Obj.transform.position = transform.position;
 
-        // ** 총알의 EnemyBullet 스크립트를 받아온다.
-        EnemyBullet Controller = Obj.AddComponent<EnemyBullet>();
-
-        // ** 총알 스크립트내부의 방향 변수를 현재 플레이어의 방향 변수로 설정 한다.
-        Controller.Direction = new Vector3(Direction, 0.0f, 0.0f);
-
-        // ** 총알 스크립트내부의 FX Prefab을 설정한다.
-        //Controller.fxPrefab = BulletPrefab;
+        // ** 총알의 BullerController 스크립트를 받아온다.
+        BulletController Controller = Obj.AddComponent<BulletController>();
 
         // ** 총알의 SpriteRenderer를 받아온다.
-        //SpriteRenderer buleltRenderer = Obj.GetComponent<SpriteRenderer>();
+        SpriteRenderer buleltRenderer = Obj.GetComponent<SpriteRenderer>();
 
         // ** 모든 설정이 종료되었다면 저장소에 보관한다.
         Bullets.Add(Obj);
 
-        while (CoolDown > 0.0f)
-        {
-            CoolDown -= Time.deltaTime;
-            yield return null;
-        }
-        Attack = false;
-        CoolDown = 5.0f;
-        
     }
 }
